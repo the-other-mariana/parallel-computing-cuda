50f75d1cdab824feba69fb9db03e54631d360d00
the-other-mariana <mariana.avalos.arce@gmail.com>
2021-08-12 08:18:25 -0500

    added labs and output
    Signed-off-by: the-other-mariana <mariana.avalos.arce@gmail.com>

---
+++ 08112021/lab01/lab01/kernel.cu
@@ -0,0 +1,47 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+
+int main()
+{
+    int device = 0; // to store the number of devices we have
+    int* count = &device;
+    cudaGetDeviceCount(count); // needs a pointer to store the result
+    // a device is a gpu card
+    printf("Device count: %d\n", device);
+
+    cudaDeviceProp properties;
+    cudaDeviceProp* pProperties = &properties;
+    cudaGetDeviceProperties(pProperties, device - 1); // device int is an index, we have one so index is zero
+    printf("Name: %s\n", properties.name); // name of the device
+    printf("multiProcessorCount: %d\n", properties.multiProcessorCount);
+    printf("maxBlocksPerMultiProcessor: %d\n", properties.maxBlocksPerMultiProcessor);
+    // the sum of all the threads in each block
+    printf("maxThreadsPerMultiProcessor: %d\n", properties.maxThreadsPerMultiProcessor);
+    // max number of threads per block
+    printf("maxThreadsPerBlock: %d\n", properties.maxThreadsPerBlock);
+
+    // Grids dimensions
+    printf("maxGridSize x axis: %d\n", properties.maxGridSize[0]); // max limit of blocks in x axis in the grid
+    printf("maxGridSize y axis: %d\n", properties.maxGridSize[1]);
+    printf("maxGridSize z axis: %d\n", properties.maxGridSize[2]);
+
+    // Block dimensions (tweak but until the multip is <= 1024)
+    printf("maxThreadsDim x axis: %d\n", properties.maxThreadsDim[0]); // max limit of threads per dimension in block
+    printf("maxThreadsDim y axis: %d\n", properties.maxThreadsDim[1]);
+    printf("maxThreadsDim z axis: %d\n", properties.maxThreadsDim[2]);
+
+    // threads in a warp
+    printf("warpSize: %d\n", properties.warpSize);
+
+    // Major Property of a GPU is the first digit of a number // 6
+    printf("major: %d\n", properties.major);
+    // Minor Property of a GPU is the second digit of a number // 1
+    printf("minor: %d\n", properties.minor);
+    // major.minor is the computing capability: in this case is 6.1
+
+    return 0;
+}
---
+++ 08112021/lab02/lab02/kernel.cu
@@ -0,0 +1,56 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int N = 8;
+    int* host_vectorA;
+    int* host_vectorB;
+    int* host_vectorC;
+
+    int* device_vectorA;
+    int* device_vectorB;
+    int* device_vectorC;
+
+    // reserve memory in the Host
+    host_vectorA = (int*)malloc(sizeof(int) * N);
+    host_vectorB = (int*)malloc(sizeof(int) * N);
+    host_vectorC = (int*)malloc(sizeof(int) * N);
+
+    // reserve memory in the Device
+    cudaMalloc((void**)&device_vectorA, sizeof(int) * N);
+    cudaMalloc((void**)&device_vectorB, sizeof(int) * N);
+
+    for (int i = 0; i < N; i++) {
+        host_vectorA[i] = i;
+    }
+
+    // data transfer: copy the host array to the device array (two different arrays)
+    cudaMemcpy(device_vectorA, host_vectorA, sizeof(int) * N, cudaMemcpyHostToDevice);
+    cudaMemcpy(device_vectorB, device_vectorA, sizeof(int) * N, cudaMemcpyDeviceToDevice);
+    cudaMemcpy(host_vectorB, device_vectorB, sizeof(int) * N, cudaMemcpyDeviceToHost);
+    cudaMemcpy(host_vectorC, host_vectorB, sizeof(int) * N, cudaMemcpyHostToHost);
+
+    printf("Vector A: ");
+    for (int i = 0; i < N; i++) {
+        printf("%d ", host_vectorA[i]);
+    }
+
+    printf("\nVector C: ");
+    for (int i = 0; i < N; i++) {
+        printf("%d ", host_vectorC[i]);
+    }
+
+    // free memory
+    free(host_vectorA);
+    free(host_vectorB);
+    free(host_vectorC);
+    cudaFree(device_vectorA);
+    cudaFree(device_vectorB);
+
+    return 0;
+}
