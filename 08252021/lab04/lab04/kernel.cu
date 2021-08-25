1918fc32685d6dacbb9002d2506ebe7dc05e0a5e
the-other-mariana <mariana.avalos.arce@gmail.com>
2021-08-25 18:05:43 -0500

    lab04 out
    Signed-off-by: the-other-mariana <mariana.avalos.arce@gmail.com>

---
+++ 08252021/lab04/lab04/kernel.cu
@@ -0,0 +1,22 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+__global__ void kernel()
+{
+    printf("threadIdx.x: %d, threadIdx.y: %d, threadIdx.z: %d | blockIdx.x: %d, blockIdx.y: %d, blockIdx.z: %d | blockDim.x: %d, blockDim.y: %d, blockDim.z: %d | gridDim.x: %d, gridDim.y: %d, gridDim.z: %d\n", 
+        threadIdx.x, threadIdx.y, threadIdx.z, 
+        blockIdx.x, blockIdx.y, blockIdx.z,
+        blockDim.x, blockDim.y, blockDim.z,
+        gridDim.x, gridDim.y, gridDim.z);
+}
+
+int main() {
+    dim3 grid(2, 2, 2);
+    dim3 block(2, 2, 2);
+    kernel <<< grid, block >>> ();
+
+    return 0;
+}
