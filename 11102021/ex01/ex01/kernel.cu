dfc9a30421f08534f1a3494ea05f11b4c5e9d6d5
the-other-mariana <mariana.avalos.arce@gmail.com>
2021-11-15 12:01:50 -0600

    report done
    Signed-off-by: the-other-mariana <mariana.avalos.arce@gmail.com>

--- 11102021/ex01/ex01/kernel.cu
+++ 11102021/ex01/ex01/kernel.cu
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <iostream>
 
-#define N 3
+#define N 32
 __constant__ int dev_A[N * N];
 
 using namespace std;
@@ -19,22 +19,26 @@ __host__ void checkCUDAError(const char* msg) {
 	}
 }
 
-__host__ void validate(int* result_CPU, int* result_GPU, int size) {
-	if (*result_CPU != *result_GPU) {
-		printf("The results are not equal.\n");
-		return;
+__host__ void validate(int* result_CPU, int* result_GPU) {
+	for (int i = 0; i < N * N; i++) {
+		if (*result_CPU != *result_GPU) {
+			printf("[FAILED] The results are not equal.\n");
+			return;
+		}
 	}
-	printf("Kernel validated successfully.\n");
+	printf("[SUCCESS] Kernel validation.\n");
 	return;
 }
 
-__host__ void CPU_fn(int* v, int* sum, const int size) {
-	for (int i = 0; i < size; i++) {
-		*sum += v[i];
+__host__ void CPU_transpose(int* vector, int* res) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			res[(i * N) + j] = vector[(N * j) + i];
+		}
 	}
 }
 
-__global__ void kernel(int* res) {
+__global__ void GPU_transpose(int* res) {
 	int gId = threadIdx.x + (blockDim.x * threadIdx.y);
 	res[gId] = dev_A[N * threadIdx.x + threadIdx.y];
 }
@@ -52,23 +56,41 @@ int main() {
 
 	int* dev_B;
 	int* host_B = (int*)malloc(sizeof(int) * N * N);
+	int* cpu_B = (int*)malloc(sizeof(int) * N * N);
 	int* host_A = (int*)malloc(sizeof(int) * N * N);
+
 	cudaMalloc((void**)&dev_B, sizeof(int) * N * N);
+	checkCUDAError("Error at cudaMalloc: dev_B");
 
 	for (int i = 0; i < N * N; i++) {
 		host_A[i] = i + 1;
 	}
 
 	cudaMemcpyToSymbol(dev_A, host_A, sizeof(int) * N * N);
+	checkCUDAError("Error at MemcpyToSymbol");
 
 	dim3 grid(1);
 	dim3 block(N, N);
-	kernel << < grid, block >> > (dev_B);
+	GPU_transpose << < grid, block >> > (dev_B);
+	checkCUDAError("Error at kernel");
 	cudaMemcpy(host_B, dev_B, sizeof(int) * N * N, cudaMemcpyDeviceToHost);
+	checkCUDAError("Error at Memcpy host_B <- dev_B");
 
+	CPU_transpose(host_A, cpu_B);
+
+	printf("Input: \n");
 	printMtx(host_A);
-	cout << endl;
+	printf("CPU: \n");
+	printMtx(cpu_B);
+	printf("GPU: \n");
 	printMtx(host_B);
+
+	validate(cpu_B, host_B);
+
+	free(host_B);
+	free(cpu_B);
+	free(host_A);
+	cudaFree(dev_B);
 
 	return 0;
 }
